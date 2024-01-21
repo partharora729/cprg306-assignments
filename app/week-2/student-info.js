0c564a911adf4d76a7fb9cd8dec55ddb9d858584
partharora729 <130489526+partharora729@users.noreply.github.com>
2024-01-21 01:13:43 -0700

    Initial commit

---
+++ app/week-2/student-info.js
@@ -0,0 +1,9 @@
+import Link from "next/link";
+export default function(){
+    return(<div>
+        <h1>CPRG 306: Web Development 2 - Assignments</h1>
+        <h1>Parth Arora</h1>
+        <Link href="">My Github Link</Link>
+    </div>)
+}
+
